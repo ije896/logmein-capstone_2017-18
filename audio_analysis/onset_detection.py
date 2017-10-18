bd5e679199d885765cc2885d5f558ee9f85ddb06
iegan <ije@umail.ucsb.edu>
2017-10-18 01:15:53 -0700

    added gitignore, calculated differences of onsets

--- audio_analysis/onset_detection.py
+++ audio_analysis/onset_detection.py
@@ -1,12 +1,18 @@
 import librosa
+import numpy as np
+
+differences = []
 
 afile = librosa.util.example_audio_file()
 
-wave, sr = librosa.load(filename)
+y, sr = librosa.load(afile)
 
 tempo, pulse_frames = librosa.beat.beat_track(y=y, sr=sr)
 
-# 4. Convert the frame indices of beat events into timestamps
 onset_times = librosa.frames_to_time(pulse_frames, sr=sr)
+for i in range(len(onset_times)-1):
+    tmp = onset_times[i+1] - onset_times[i]
+    differences.append(tmp)
 
-print(onset_times)
+print("onsets: ", onset_times)
+print("differences: ", differences)
