e3241b1d9d0e9cb1dc7ad6b2119fd6ca71913d94
iegan <ije@umail.ucsb.edu>
2017-10-23 02:57:40 -0700

    Tested onset detection with speech file. Edited .gitignore

--- audio_analysis/onset_detection.py
+++ audio_analysis/onset_detection.py
@@ -1,18 +1,41 @@
 import librosa
+import librosa.display
 import numpy as np
+import matplotlib.pyplot as plt
 
 differences = []
 
-afile = librosa.util.example_audio_file()
+afile = '/Users/iegan/Documents/School/F17 Classes/CS189A/logmein-capstone_2017-18/audio_samples/speech_sample.wav'
+    #librosa.util.example_audio_file()
 
 y, sr = librosa.load(afile)
 
-tempo, pulse_frames = librosa.beat.beat_track(y=y, sr=sr)
+#tempo, pulse_frames = librosa.beat.beat_track(y=y, sr=sr)
+onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
+onset_times = librosa.frames_to_time(onset_frames, sr=sr)
 
-onset_times = librosa.frames_to_time(pulse_frames, sr=sr)
-for i in range(len(onset_times)-1):
-    tmp = onset_times[i+1] - onset_times[i]
-    differences.append(tmp)
+# for i in range(len(onset_times)-1):
+#     tmp = onset_times[i+1] - onset_times[i]
+#     differences.append(tmp)
 
 print("onsets: ", onset_times)
-print("differences: ", differences)
+#print("differences: ", differences)
+
+o_env = librosa.onset.onset_strength(y, sr=sr)
+times = librosa.frames_to_time(np.arange(len(o_env)), sr=sr)
+onset_frames = librosa.onset.onset_detect(onset_envelope=o_env, sr=sr)
+
+D = librosa.stft(y)
+plt.figure()
+ax1 = plt.subplot(2, 1, 1)
+librosa.display.specshow(librosa.amplitude_to_db(D, ref=np.max),
+                         x_axis='time', y_axis='log')
+plt.title('Power spectrogram')
+plt.subplot(2, 1, 2, sharex=ax1)
+plt.plot(times, o_env, label='Onset strength')
+plt.vlines(times[onset_frames], 0, o_env.max(), color='r', alpha=0.9,
+           linestyle='--', label='Onsets')
+plt.axis('tight')
+plt.legend(frameon=True, framealpha=0.75)
+
+plt.show()
