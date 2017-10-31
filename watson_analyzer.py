517a2b958f283cb8df498d2f6dfc278d2af225ab
Ryan Allen <rmallensb@gmail.com>
2017-10-31 01:42:17 -0700

    Started using plotly to graph tone analysis, but I think plotly also requires a subscription.  Going to look into new alternatives tomorrow

--- watson_analyzer.py
+++ watson_analyzer.py
@@ -5,6 +5,8 @@ import sys
 import requests
 import json
 from watson_developer_cloud import ToneAnalyzerV3
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 # Notes from API Reference
 # https://www.ibm.com/watson/developercloud/tone-analyzer/api/v3/?python#error-handling
@@ -25,7 +27,64 @@ def tone_analyzer(text):
 	# content_type: content type of request, text/plain, text/html, application/json (default text/plain)
 	tone = tone_analyzer.tone(text, tones='emotion, social', sentences='true', content_type='text/plain')
 
-	print(json.dumps(tone, indent=2))
+	json_tone = json.dumps(tone, indent=2)
+	#print 
+	#print(json_tone)
+	return tone
+
+def graph_it(input_dict1, input_dict2):
+	names1 = []
+	values1 = []
+	names2 = []
+	values2 = []
+
+	for key in input_dict1:
+		names1.append(key)
+		values1.append(input_dict1[key])
+
+	for key in input_dict2:
+		names2.append(key)
+		values2.append(input_dict2[key])
+
+	plot1 = go.Area(
+		r=names1,
+		t=values1,
+		name='emotion',
+		marker=dict(
+			color = 'rgb(0,0,255'
+		)
+	)
+
+	plot2 = go.Area(
+		r=names2,
+		t=values2,
+		name='social',
+		marker=dict(
+			color = 'rgb(255,0,0'
+		)
+	)
+
+	plot_data = [plot1, plot2]
+
+	layout = go.Layout(
+    title='emotion',
+    font=dict(
+        size=16
+    ),
+    legend=dict(
+        font=dict(
+            size=16
+        )
+    ),
+    radialaxis=dict(
+        ticksuffix='%'
+    ),
+    orientation=270
+	)
+
+	fig = go.Figure(data=plot_data, layout=layout)
+	py.iplot(fig, filename='emotion')
+
 
 
 
@@ -36,7 +95,26 @@ def main():
 		print("Usage: python3 %s input_file (max 128kB) ", sys.argv[0])
 		exit(1)
 
-	tone_analyzer(text)
+	tone = tone_analyzer(text)
+
+	emotion_tone =  tone["document_tone"]["tone_categories"][0]["tones"]
+	social_tone =  tone["document_tone"]["tone_categories"][1]["tones"]
+
+	emotion_dict = {}
+	social_dict = {}
+
+	for item in emotion_tone:
+		emotion_dict[item["tone_name"]] = item["score"]
+
+	for item in social_tone:
+		social_dict[item["tone_name"]] = item["score"]
+
+
+	#print(emotion_dict)
+	#print(social_dict)
+
+	graph_it(emotion_dict, social_dict)
+
 
 
 if __name__ == '__main__':
