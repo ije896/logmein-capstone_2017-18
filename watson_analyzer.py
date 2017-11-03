2b681976a40209c26c917bdb33c0d8de679189c5
Ryan Allen <rmallensb@gmail.com>
2017-11-02 17:43:29 -0700

    Removed graphing from watson_analyzer, going to use Zach's radio grapher

--- watson_analyzer.py
+++ watson_analyzer.py
@@ -5,15 +5,11 @@ import sys
 import requests
 import json
 from watson_developer_cloud import ToneAnalyzerV3
-import plotly.plotly as py
-import plotly.graph_objs as go
 
 # Notes from API Reference
 # https://www.ibm.com/watson/developercloud/tone-analyzer/api/v3/?python#error-handling
 
-
-# This might not work anymore
-# Unsure if the credentials are wiped
+# These credentials were made with a trial account
 def tone_analyzer(text):
 	tone_analyzer = ToneAnalyzerV3(
 		version='2016-05-19',
@@ -28,64 +24,11 @@ def tone_analyzer(text):
 	tone = tone_analyzer.tone(text, tones='emotion, social', sentences='true', content_type='text/plain')
 
 	json_tone = json.dumps(tone, indent=2)
-	#print 
-	#print(json_tone)
-	return tone
+	
+	tone_tuple = {json_tone, tone}
 
-def graph_it(input_dict1, input_dict2):
-	names1 = []
-	values1 = []
-	names2 = []
-	values2 = []
-
-	for key in input_dict1:
-		names1.append(key)
-		values1.append(input_dict1[key])
-
-	for key in input_dict2:
-		names2.append(key)
-		values2.append(input_dict2[key])
-
-	plot1 = go.Area(
-		r=names1,
-		t=values1,
-		name='emotion',
-		marker=dict(
-			color = 'rgb(0,0,255'
-		)
-	)
-
-	plot2 = go.Area(
-		r=names2,
-		t=values2,
-		name='social',
-		marker=dict(
-			color = 'rgb(255,0,0'
-		)
-	)
-
-	plot_data = [plot1, plot2]
-
-	layout = go.Layout(
-    title='emotion',
-    font=dict(
-        size=16
-    ),
-    legend=dict(
-        font=dict(
-            size=16
-        )
-    ),
-    radialaxis=dict(
-        ticksuffix='%'
-    ),
-    orientation=270
-	)
-
-	fig = go.Figure(data=plot_data, layout=layout)
-	py.iplot(fig, filename='emotion')
-
-
+	print(json_tone)
+	return tone_tuple
 
 
 def main():
@@ -95,7 +38,9 @@ def main():
 		print("Usage: python3 %s input_file (max 128kB) ", sys.argv[0])
 		exit(1)
 
-	tone = tone_analyzer(text)
+	# Returns a tuple
+	# {json, dictionary}
+	tone_tuple =  = tone_analyzer(text)
 
 	emotion_tone =  tone["document_tone"]["tone_categories"][0]["tones"]
 	social_tone =  tone["document_tone"]["tone_categories"][1]["tones"]
@@ -103,6 +48,7 @@ def main():
 	emotion_dict = {}
 	social_dict = {}
 
+	# Parse items out of 
 	for item in emotion_tone:
 		emotion_dict[item["tone_name"]] = item["score"]
 
@@ -110,11 +56,8 @@ def main():
 		social_dict[item["tone_name"]] = item["score"]
 
 
-	#print(emotion_dict)
-	#print(social_dict)
-
-	graph_it(emotion_dict, social_dict)
-
+	print(emotion_dict)
+	print(social_dict)
 
 
 if __name__ == '__main__':
