6ef3f4824fe640578b7122a486fc1b5f19070ad1
iegan <ije@umail.ucsb.edu>
2017-11-29 14:07:43 -0800

    Pitch from autocorrelation is working (but slow)

--- audio/pitch_detection.py
+++ audio/pitch_detection.py
@@ -11,11 +11,11 @@ cscale = '/Users/iegan/Downloads/c_scale.wav'
 speech_file_short = '/Users/iegan/Documents/School/F17 Classes/CS189A/logmein-capstone_2017-18/audio_samples/halloween.wav'
 
 speech = '/Users/iegan/Documents/School/F17 Classes/CS189A/logmein-capstone_2017-18/audio_samples/speech_sample.wav'
-y, sr = librosa.load(cscale)
-pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
+y, sr = librosa.load(speech)
+#pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
 hop_length = 100
 
-idx = (magnitudes>0)
+#idx = (magnitudes>0)
 
 onset_samples = librosa.onset.onset_detect(y,
                                          sr=sr, units='samples',
@@ -28,6 +28,7 @@ onset_samples = librosa.onset.onset_detect(y,
                                          delta=0.2,
                                          wait=0)
 onset_boundaries = numpy.concatenate([[0], onset_samples, [len(y)]])
+onset_times = librosa.samples_to_time(onset_boundaries)
 
 inds = []
 pitch = []
@@ -57,18 +58,32 @@ def estimate_pitch(segment, sr, fmin=50.0, fmax=2000.0):
   f0 = float(sr)/i
   return f0
 
+# could store enumeration of elements in list to remove
+def reject_outliers(data, indeces, m = 2.):
+  d = numpy.abs(data - numpy.median(data))
+  mdev = numpy.median(d)
+  s = d/mdev if mdev else 0.
+  return data[s<m], indeces[s<m]
+
+
 def estimate_pitch_for_segment(x, onset_samples, i, sr):
-    n0 = onset_samples[i]
-    n1 = onset_samples[i+1]
-    f0 = estimate_pitch(x[n0:n1], sr)
-    return f0
+  n0 = onset_samples[i]
+  n1 = onset_samples[i+1]
+  f0 = estimate_pitch(x[n0:n1], sr)
+  return f0
 autocorred = []
 for i in range(len(onset_boundaries)-1):
-  autocorred.append((onset_samples[i-1],estimate_pitch_for_segment(y, onset_boundaries, i, sr=sr)))
+  autocorred.append(estimate_pitch_for_segment(y, onset_boundaries, i, sr=sr))
 
+autocorred = numpy.asarray(autocorred)
+onset_times = numpy.delete(onset_times, 0, 0)
+autocorred, onset_times = reject_outliers(autocorred, onset_times)
 
+final = list(zip(onset_times, autocorred))
 
-plt.plot(autocorred)
+print(final)
+
+plt.plot(*zip(*final))
 plt.show()
 
 
