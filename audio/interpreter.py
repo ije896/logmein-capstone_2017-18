d692a52f91a7e48a93a4243fc284cdf484332b99
iegan <ije@umail.ucsb.edu>
2018-02-19 13:45:50 -0800

    Removed my temp workspace files

--- audio/interpreter.py
+++
@@ -1,6 +0,0 @@
-import interface
-# speech_file = '/Users/iegan/Documents/School/W18 Classes/CS189B/logmein-capstone_2017-18/audio_media/speech_audio/enigma_tc_snippet.wav'
-speech_file = '/Users/iegan/Documents/School/W18 Classes/CS189B/logmein-capstone_2017-18/audio_media/nonsense.wav'
-opts = {'run_all':True}
-a = interface.Interface()
-a.process_filepath(speech_file, opts)
